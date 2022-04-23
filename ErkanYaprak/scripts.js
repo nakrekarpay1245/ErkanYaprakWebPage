9befdb4aaff50922239e36f8b4cfe986435f5ab8
nakrekarpay1245 <rknyprk2001@gmail.com>
2022-04-23 10:32:47 +0300

    23.04.2022
    First commit

---
+++ ErkanYaprak/scripts.js
@@ -0,0 +1,10 @@
+VanillaTilt.init(document.querySelectorAll(".social__links ul li"), {
+    max: 10,
+    speed: 800,
+    glare: true,
+    "max-glare": 1
+});
+
+function Download(url) {
+    window.open(url)
+}
